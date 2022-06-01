fe25bfbc4ad9b58d3b647e9c00e7f3b65a1abdb2
travisback <travisback@icloud.com>
2022-05-31 23:51:14 -0700

    Initial check-in of module CSharp_Day002_TipCalculator

---
+++ CSharp_Day002_TipCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Day002_TipCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
